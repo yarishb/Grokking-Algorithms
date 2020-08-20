a1077d54fa1cb5739bff99d960b883a652f8cd65
yarishb <bogdan040105@gmail.com>
2020-08-20 10:10:00 +0300

    1 Theme. Binary Search

---
+++ 1. BinarySearch/binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(arr, num):
+     start = 0
+     end = len(arr) -1
+
+     while start <= end:
+          mid = (start + end)
+          guess = arr[mid]
+
+          if guess == num:
+               return mid
+          if guess > num:
+               end = mid - 1
+          else:
+               start = mid - 1
+          
+     return None
+
+list = [1, 3, 5, 7, 8, 9]
+
+print (binary_search(list, 8))
+print (binary_search(list, -1))
